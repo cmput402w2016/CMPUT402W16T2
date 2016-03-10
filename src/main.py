2244424ce9941132afda6da0f119cf9eb5fb9d9a
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-03-10 11:49:12 -0700

    fix the module directory. Add argparse for parsing video sourse

--- src/main.py
+++ src/main.py
@@ -1,26 +1,34 @@
 # the minimal main.py program to load the video controller, log controller.
 # runs the program adhering to MVC requirements
-from src.views.expanded_tk import TkWindowViewer
-from src.controllers.videocontroller import VideoController
-from src.controllers.logcontroller import LogController
+
+import argparse
+from views.expanded_tk import TkWindowViewer
+from controllers.videocontroller import VideoController
+from controllers.logcontroller import LogController
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", type=str, help="path to the video file", required=True)
+args = vars(ap.parse_args())
+
 DEBUG = True
 #RUN_GUI = DEBUG
 
 PLAY_STATUS = True
-PLAY_STRING = "Play" if PLAY_STATUS else "Stop" 
+PLAY_STRING = "Play" if PLAY_STATUS else "Stop"
 
 def main():
     global PLAY_STATUS
     maintk = TkWindowViewer()
     maintk.play_stop_string.set(PLAY_STRING)
     # TODO: Do not hardcode this
-    vc = VideoController("W:/Troy/Development/CMPUT402W16T2/videos/sample_video_3.mp4")
+    vc = VideoController(args["video"])
     lc = LogController()
-    
+
     (frame, count) = vc.runIteration()
- 
+
     maintk.setDisplayImg(frame)
-    
+
     maintk.runMainloop()
 
 if __name__ == "__main__":
