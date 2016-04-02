4938889ff37d02db2d846a3808cf5a8a7c517c83
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-04-02 16:57:04 -0600

    post packet to server

--- src/controllers/logcontroller.py
+++ src/controllers/logcontroller.py
@@ -1,20 +1,27 @@
 import json
 import datetime
+import time;
 import os.path
+import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 class LogController:
     """
     One log Controller is instantiated in the constructor of each
     video controller.
-    
+
     The log controller builds JSON packets of data that it appends
     to a JSON dump it holds a reference to while it is being written.
-    
+
     Every time the data is dumped, it is sorted by time so that it
     remains in the order it was written
     """
     def __init__(self):
-        
+
         fname = '%s.log' % datetime.datetime.now().strftime("%Y_%m_%d %H_%M_%S")
         #go up through controllers,src to log folder
         base_folder = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
@@ -23,27 +30,48 @@ class LogController:
             json.dump([], f)
 
     def writeToLog(self, timestamp, count):
-        
+
         with open(self.filename) as f:
             data = json.load(f)
         if (len(data) >= 100):
             data.pop()
-            
+
         packet = self._createJSON(timestamp,count)
-        #data.append(self._createJSON(timestamp,count))
+        if ( self._postToServer(packet) ):
+            packet['success'] = 1
+        else:
+            packet['success'] = 0
         data.append(packet)
 
         data.sort(key=self._getKey, reverse=True)
 
         with open(self.filename, 'w') as f:
             json.dump(data, f)
-        
+
         return packet
+
+    def _postToServer(self, packet):
+        # TODO: 'from' & 'to' should not be hardcoded here
+        pattern = '%Y/%m/%d %H:%M:%S' # the pattern should be the same as the one on videocontroller.py
+        epoch = int(time.mktime(time.strptime(packet['time'], pattern)))
+        url = "http://199.116.235.225:8000/traffic"
+        data = {}
+        data['from'] = "c3x2wb3240dw"
+        data['to'] = "c3x2wb331v77"
+        data['key'] = "TESTING_POST"
+        data['timestamp'] = epoch
+        data['value'] = packet['count']
+        r = requests.post(url, json=data)
+        # logger.info(r.json())
+        if (r.status_code == 201):
+            return True
+        else:
+            return False
 
     def _createJSON(self, time, count):
         packet = {}
         packet['time'] = time
-        packet['count'] = count
+        packet['count'] = round(count, 1)
         return packet
 
     def _getKey(self, item):
